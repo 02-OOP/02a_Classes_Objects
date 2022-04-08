f723a0924464388854f22984e759a53093f9c984
Peter <schmidt.multimedia@gmail.com>
2022-04-08 11:04:40 +0200

    Objekte / reference / this

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,16 +1,29 @@
 package com.cc.java;
 
-public class App {
-    
-    public static void main(String[] args) {
-     
+public class App 
+{
+    public static void main(String[] args) 
+    {
+        Cat cat = new Cat();
+        output("Blick von aussen: " + cat);
+        output("Blick von innen: " + cat.tellYourAddress());
+        output("----------------");
+
+        Cat cat1 = new Cat();
+        output("Blick von aussen: " + cat1);
+        output("Blick von innen: " + cat1.tellYourAddress());
+        output("----------------");    
+
     }
 
-    public static void output(String outputStr) {
+
+
+    public static void output(String outputStr) 
+    {
         System.out.println(outputStr);
     }
 
-
-
 }
+
+
 
